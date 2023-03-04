86ba26bf4fc90a8bd9de47f70f0f4947cf64342b
wendar <c.cortescandela@gmail.com>
2023-03-04 23:31:39 +0100

    Cambio funciones declarativas por expresivas

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 const navEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 
-navEmail.addEventListener('click', toggleDesktopMenu)
-
-function toggleDesktopMenu() {
+const toggleDesktopMenu = () => {
     desktopMenu.classList.toggle('inactive')
 }
+
+navEmail.addEventListener('click', toggleDesktopMenu)
