961ec9142c0a67a56e7a32103b656fbdd1546d77
wendar <c.cortescandela@gmail.com>
2023-03-05 14:36:41 +0100

    Añado menu mobile

--- main.js
+++ main.js
@@ -2,9 +2,15 @@
 
 const navEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const burgerMenu = document.querySelector('.menu')
 
 const toggleDesktopMenu = () => {
-    desktopMenu.classList.toggle('inactive')
+    desktopMenu.classList.toggle('profile-inactive')
 }
-
 navEmail.addEventListener('click', toggleDesktopMenu)
+
+const toggleMobileMenu = () => {
+    mobileMenu.classList.toggle('mobile-inactive')
+}
+burgerMenu.addEventListener('click', toggleMobileMenu)
