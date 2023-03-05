4ab43c7c61b9d20abc8ea3006f93f30b786be966
wendar <c.cortescandela@gmail.com>
2023-03-05 18:55:43 +0100

    Añadimos shopping cart y js para evitar solapamientos

--- main.js
+++ main.js
@@ -4,13 +4,24 @@ const navEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const burgerMenu = document.querySelector('.menu')
+const carritoIcon = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail')
 
-const toggleDesktopMenu = () => {
+const showDesktopMenu = () => {
+    productDetail.classList.add('product-detail-inactive')
     desktopMenu.classList.toggle('profile-inactive')
 }
-navEmail.addEventListener('click', toggleDesktopMenu)
+navEmail.addEventListener('click', showDesktopMenu)
 
-const toggleMobileMenu = () => {
+const showProductDetail = () => {
+    desktopMenu.classList.add('profile-inactive')
+    productDetail.classList.toggle('product-detail-inactive')
+    mobileMenu.classList.add('mobile-inactive')
+}
+carritoIcon.addEventListener('click', showProductDetail)
+
+const showMobileMenu = () => {
+    productDetail.classList.add('product-detail-inactive')
     mobileMenu.classList.toggle('mobile-inactive')
 }
-burgerMenu.addEventListener('click', toggleMobileMenu)
+burgerMenu.addEventListener('click', showMobileMenu)
